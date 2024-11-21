d2f9122441192a95618ff4d5518287a9a8eab5bb
Muhammad Ahmed Raza <mahmedraza444@gmail.com>
2024-11-21 13:07:06 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import './index.css'
+import Pokemon from './Pokemon'
+
+function App() {
+
+  return (
+    <>
+      <Pokemon />   
+    </>
+  )
+}
+export default App
